e2bce11b69855b9e03b2f0b8f3eb7cbbab714074
Simon Li <simon.li@shiptheory.com>
2023-12-02 21:37:13 +0000

    Day 1

--- day1/main.php
+++ day1/main.php
@@ -1,12 +1,20 @@
 <?php
-
 define('DS', DIRECTORY_SEPARATOR);
 
-$filename = getcwd() . DS. 'datafile.txt';
-echo $filename;
+$filename = 'datafile.txt';
+$file = file(getcwd() . DS. $filename);
 
-$file = file($filename);
-
+$total = 0;
 foreach ($file as $line) {
-    echo $line;
+    preg_match('/(\d).*(\d)/', $line, $matches);
+    if (count($matches) == 3) {
+        $digits = (int) $matches[1] . $matches[2];
+    } else {
+        preg_match('/(\d)/', $line, $matches);
+        $digits = (int) !empty($matches) ? $matches[0] . $matches[1] : 0 ;
+    }
+
+    $total += $digits;
 }
+
+echo "Total: $total " . PHP_EOL;
