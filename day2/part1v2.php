6c9f78e039214999f48529c9983711e1fcc7eb8a
Simon Li <simon.li@shiptheory.com>
2023-12-10 15:04:05 +0000

    Working v2

--- day2/part1v2.php
+++ day2/part1v2.php
@@ -1,16 +1,18 @@
 <?php
 
 $bag = ['red' => 12, 'green' => 13, 'blue' => 14];
-
 $lines = file(getcwd() . DIRECTORY_SEPARATOR. 'datafile.txt');
+// $lines = file(getcwd() . DIRECTORY_SEPARATOR. 'test.txt');
 
-$answer = 0; // 2943 (need bigger!),
+// 3035, 66027
+list ($answer1, $answer2) = [0, 0];
+
 for ($i = 0; $i < count($lines); $i++) {
     $sections = explode(':', rtrim($lines[$i]));
     $game_num = str_replace('Game ', '', $sections[0]);
     $tries = explode(';', trim($sections[1]));
 
-    $good = true;
+    list ($good, $balls_max) = [true, []];
     foreach ($tries as $try) {
         $balls = explode(',', trim($try));
         array_map(function($ball) use (&$balls_new) {
@@ -18,25 +20,32 @@ for ($i = 0; $i < count($lines); $i++) {
             $balls_new[trim($parts[1])] = trim($parts[0]);
         }, $balls);
 
-        $good = $good && validateAgainstBag($balls_new, $bag);
+        // oh! && will not work here !
+        $good = $good and validateAgainstBag($balls_new, $bag, $balls_max);
         unset($balls_new);
-        if (!$good) {
-            break;
-        }
     }
 
-    $answer += $good ? (int) $game_num : 0;
-    echo "Game $game_num: good = " . (int) $good . ". Answer $answer. " .PHP_EOL;
+    $answer1 += $good ? (int) $game_num : 0;
+    $ans2 = array_reduce(array_values($balls_max), "product", 1);
+    $answer2 += $ans2;
+
+    echo "Game $game_num: good = " . (int) $good . ". Answer 1 = $answer1. " .PHP_EOL;
+    echo "Game $game_num: good = " . (int) $good . ". Answer 2 = $answer2. ans2  = $ans2 " .PHP_EOL;
+    unset($balls_max);
 }
 
-function validateAgainstBag($balls_new, $bag) {
+function validateAgainstBag($balls_new, $bag, &$balls_max) {
+    $valid = true;
     foreach ($balls_new as $color => $count) {
-        if (!isset($bag[$color])) {
-            return false;
-        } else if ((int) $count > $bag[$color]) {
-            return false;
+        if ($count > $bag[$color]) {
+            $valid = false;
         }
+        $balls_max[$color] = (!isset($balls_max[$color]) ? $count : max($balls_max[$color], $count));
     }
-    return true;
+    return $valid;
 }
 
+function product($carry, $item)
+{
+    return ($carry *= $item);
+}
