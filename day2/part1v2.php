dc23d16708294c082e253e26bd6bc70af39d4832
Simon Li <simon.li@shiptheory.com>
2023-12-10 00:24:43 +0000

    fix unset bug

--- day2/part1v2.php
+++ day2/part1v2.php
@@ -1,11 +1,6 @@
 <?php
 
-// 12 red cubes, 13 green cubes, and 14 blue cubes
-$bag = [
-    'red' => 12,
-    'green' => 13,
-    'blue' => 14
-];
+$bag = ['red' => 12, 'green' => 13, 'blue' => 14];
 
 $lines = file(getcwd() . DIRECTORY_SEPARATOR. 'datafile.txt');
 
@@ -16,7 +11,6 @@ for ($i = 0; $i < count($lines); $i++) {
     $tries = explode(';', trim($sections[1]));
 
     $good = true;
-    $all_trial = [];
     foreach ($tries as $try) {
         $balls = explode(',', trim($try));
         array_map(function($ball) use (&$balls_new) {
@@ -25,25 +19,24 @@ for ($i = 0; $i < count($lines); $i++) {
         }, $balls);
 
         $good = $good && validateAgainstBag($balls_new, $bag);
+        unset($balls_new);
         if (!$good) {
-            // echo PHP_EOL . 'Bad. ' . json_encode($balls_new) . PHP_EOL;
             break;
         }
-        unset($balls_new);
     }
 
-    $answer += $good ? $game_num : 0;
-    echo $lines[$i] . "Answer $answer. Game $game_num: good = " . (int) $good . PHP_EOL . PHP_EOL;
-    // if ($i == 5) { break; }
+    $answer += $good ? (int) $game_num : 0;
+    echo "Game $game_num: good = " . (int) $good . ". Answer $answer. " .PHP_EOL;
 }
 
 function validateAgainstBag($balls_new, $bag) {
     foreach ($balls_new as $color => $count) {
         if (!isset($bag[$color])) {
             return false;
-        } else if ($count > (int) $bag[$color]) {
+        } else if ((int) $count > $bag[$color]) {
             return false;
         }
     }
     return true;
 }
+
